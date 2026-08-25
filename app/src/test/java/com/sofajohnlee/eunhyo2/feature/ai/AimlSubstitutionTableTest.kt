a485ff14b06229920e00d3223b9787522b8dc3fa
sofajohnlee <sofajohnlee@gmail.com>
2026-08-26 08:54:01 +0900

    test: cover AIML substitution tables

---
+++ app/src/test/java/com/sofajohnlee/eunhyo2/feature/ai/AimlSubstitutionTableTest.kt
@@ -0,0 +1,24 @@
+package com.sofajohnlee.eunhyo2.feature.ai
+
+import java.io.ByteArrayInputStream
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class AimlSubstitutionTableTest {
+    @Test
+    fun appliesProgramAbStyleSubstitutions() {
+        val source = "\" can't \" ,\" can not \"\n\" i'm \" ,\" I am \"\n"
+        val table = AimlSubstitutionTable.parse(ByteArrayInputStream(source.toByteArray()))
+
+        assertEquals("I can not go", table.apply("I can't go"))
+        assertEquals("I am ready", table.apply("I'm ready"))
+    }
+
+    @Test
+    fun appliesPersonSubstitutionWithoutBreakingWhitespace() {
+        val source = "\" you \" ,\" me \"\n\" your \" ,\" my \"\n"
+        val table = AimlSubstitutionTable.parse(ByteArrayInputStream(source.toByteArray()))
+
+        assertEquals("me and my book", table.apply("you and your book"))
+    }
+}
