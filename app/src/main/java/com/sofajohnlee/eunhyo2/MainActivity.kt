b5a0c5922d7cd70ad862cd85c9900d128ead30d4
sofajohnlee <sofajohnlee@gmail.com>
2026-08-26 07:15:23 +0900

    feat: connect Korean study from launcher

--- app/src/main/java/com/sofajohnlee/eunhyo2/MainActivity.kt
+++ app/src/main/java/com/sofajohnlee/eunhyo2/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import com.sofajohnlee.eunhyo2.databinding.ActivityMainBinding
 import com.sofajohnlee.eunhyo2.feature.english.EnglishStudyActivity
+import com.sofajohnlee.eunhyo2.feature.korean.KoreanStudyActivity
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
@@ -25,6 +26,9 @@ class MainActivity : AppCompatActivity() {
         binding.buttonHigh.setOnClickListener { viewModel.selectGrade(GradeLevel.HIGH) }
         binding.buttonEnglishStudy.setOnClickListener {
             startActivity(Intent(this, EnglishStudyActivity::class.java))
+        }
+        binding.buttonKoreanStudy.setOnClickListener {
+            startActivity(Intent(this, KoreanStudyActivity::class.java))
         }
 
         lifecycleScope.launch {
