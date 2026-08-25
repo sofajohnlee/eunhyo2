8ed2f06ea4c0a3826375b2fc7509f250bb3b0d5a
sofajohnlee <sofajohnlee@gmail.com>
2026-08-26 07:10:03 +0900

    refactor: model supported study languages

---
+++ app/src/main/java/com/sofajohnlee/eunhyo2/domain/model/StudyLanguage.kt
@@ -0,0 +1,10 @@
+package com.sofajohnlee.eunhyo2.domain.model
+
+enum class StudyLanguage(val localeTag: String) {
+    KOREAN("ko-KR"),
+    ENGLISH("en-US"),
+    FRENCH("fr-FR"),
+    CHINESE("zh-CN"),
+    JAPANESE("ja-JP"),
+    ITALIAN("it-IT"),
+}
